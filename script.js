3f28d06ff543bebc3b0e59a44f0167ed0454de1f
Valeriia Isaienko <93624578+Magic1vy@users.noreply.github.com>
2023-06-10 19:04:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 window.onload = function() {
-    if (document.cookie.indexOf('cookieConsent=true') === -1 && document.cookie.indexOf('declineCookieConsent=true') === -1) {
+    if (!getCookie('cookieConsent') && !getCookie('declineCookieConsent')) {
         document.getElementById('cookieConsentPopup').style.display = 'block';
     } else {
         document.getElementById('cookieConsentPopup').style.display = 'none';
         document.body.classList.remove('modal-open');
     }
 };
+
+function setCookie(name, value, days) {
+    let expires = '';
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = '; expires=' + date.toUTCString();
+    }
+    document.cookie = name + '=' + value + expires + '; path=/';
+}
+
+function getCookie(name) {
+    let nameEQ = name + '=';
+    let cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) === 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
+}
+
 function closePopupAndSetCookie() {
     document.getElementById('cookieConsentPopup').style.display = 'none';
-
-    let date = new Date();
-    date.setFullYear(date.getFullYear() + 1); // Cookie expiration - 1 year
-    document.cookie = "cookieConsent=true; expires=" + date.toUTCString() + "; path=/";
+    setCookie('cookieConsent', 'true', 365);
 }
 
 function declineAndSetCookie() {
     document.getElementById('cookieConsentPopup').style.display = 'none';
-
-    let date = new Date();
-    date.setFullYear(date.getFullYear() + 1); // Cookie expiration - 1 year
-    document.cookie = "declineCookieConsent=true; expires=" + date.toUTCString() + "; path=/";
+    setCookie('declineCookieConsent', 'true', 365);
 }
 
 function showCookiePolicy() {
@@ -33,12 +53,6 @@ function closeCookiePolicyModal() {
     document.getElementById('cookiePolicyModal').style.display = 'none';
     document.body.classList.remove('modal-open');
 }
-
-window.onload = function() {
-    if (document.cookie.indexOf('cookieConsent=true') === -1 && document.cookie.indexOf('declineCookieConsent=true') === -1) {
-        document.getElementById('cookieConsentPopup').style.display = 'block';
-    }
-};
 
 
 
