0008fb396c82729626501abe945f57c0407f4b69
Valeriia Isaienko <93624578+Magic1vy@users.noreply.github.com>
2023-06-10 18:51:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
+function closePopupAndSetCookie() {
+    document.getElementById('cookieConsentPopup').style.display = 'none';
 
-// function closePopupAndSetCookie() {
-//     document.getElementById('cookieConsentPopup').style.display = 'none';
+    let date = new Date();
+    date.setFullYear(date.getFullYear() + 1); // Cookie expiration - 1 year
+    document.cookie = "cookieConsent=true; expires=" + date.toUTCString() + "; path=/";
+}
 
-//     let date = new Date();
-//     date.setFullYear(date.getFullYear() + 1); // Срок действия cookie - 1 год
-//     document.cookie = "cookieConsent=true; expires=" + date.toUTCString() + "; path=/";
-// }
+function declineAndSetCookie() {
+    document.getElementById('cookieConsentPopup').style.display = 'none';
 
-// function declineAndSetCookie() {
-//     document.getElementById('cookieConsentPopup').style.display = 'none';
+    let date = new Date();
+    date.setFullYear(date.getFullYear() + 1); // Cookie expiration - 1 year
+    document.cookie = "declineCookieConsent=true; expires=" + date.toUTCString() + "; path=/";
+}
 
-//     let date = new Date();
-//     date.setFullYear(date.getFullYear() + 1); // Срок действия cookie - 1 год
-//     document.cookie = "declineCookieConsent=true; expires=" + date.toUTCString() + "; path=/";
-// }
+function showCookiePolicy() {
+    document.getElementById('cookieConsentPopup').style.display = 'none';
+    document.getElementById('cookiePolicyModal').style.display = 'block';
+    document.getElementById('cookiePolicyModalContent').style.display = 'block';
+    document.body.classList.add('modal-open');
+}
 
-// window.onload = function() {
-//     if (document.cookie.indexOf('cookieConsent=true') === -1 && document.cookie.indexOf('declineCookieConsent=true') === -1) {
-//         document.getElementById('cookieConsentPopup').style.display = 'block';
-//     }
-// }
+function closeCookiePolicyModal() {
+    document.getElementById('cookiePolicyModal').style.display = 'none';
+    document.body.classList.remove('modal-open');
+}
+
+window.onload = function() {
+    if (document.cookie.indexOf('cookieConsent=true') === -1 && document.cookie.indexOf('declineCookieConsent=true') === -1) {
+        document.getElementById('cookieConsentPopup').style.display = 'block';
+    }
+};
+
+
 
 const menuBtn = document.getElementById('menuBtn');
 const sidenav = document.getElementById('mySidenav');
@@ -57,9 +70,13 @@ for (i = 0; i < accordion.length; i++) {
     });
 }
 
-AOS.init({
-    duration: 2000,
-    easing: 'ease-in-out',
-});
+if (window.innerWidth > 768) {
+    AOS.init({
+        duration: 2000,
+        easing: 'ease-in-out',
+    });
+}
+
+
 
 
